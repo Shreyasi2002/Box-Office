77e520f53f1f3c91ccfe3d6f907bd84278ecc9e0
Shreyasi2002 <shreyasi18sia@gmail.com>
2021-10-16 13:03:51 +0530

    Creating search bar -- Working with TVMaze API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
-const Home = () => <MainPageLayout>This is HOME page</MainPageLayout>;
+const Home = () => {
+    const [input, setInput] = useState('');
 
+    const onInputChange = ev => {
+        setInput(ev.target.value);
+    };
+
+    const onSearch = () => {
+        // Call browser API to fetch remote data
+
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r =>
+            r.json()
+        );
+    };
+
+    const onKeyDown = ev => {
+        if (ev.keyCode === 13) {
+            onSearch();
+        }
+    };
+
+    return (
+        <MainPageLayout>
+            This is HOME page
+            <br />
+            <br />
+            <input
+                type="text"
+                onChange={onInputChange}
+                onKeyDown={onKeyDown}
+                value={input}
+            />
+            <button type="button" onClick={onSearch}>
+                Search
+            </button>
+        </MainPageLayout>
+    );
+};
 export default Home;
