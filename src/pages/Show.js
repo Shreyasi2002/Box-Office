6d1d3c9b043ee983dbf984e73cb99e64ef2d3304
Shreyasi2002 <shreyasi18sia@gmail.com>
2021-10-17 12:30:53 +0530

    Loading... message and errors

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,13 +5,41 @@ import { apiGet } from '../misc/config';
 const Show = () => {
     const [show, setShow] = useState(null);
     const { id } = useParams();
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [errors, setErrors] = useState(null);
+
     useEffect(() => {
-        apiGet(
-            `/shows/${id}?embed[]=seasons&embed[]=episodes&embed[]=cast`
-        ).then(results => {
-            setShow(results);
-        });
+        let isMounted = true;
+
+        apiGet(`/shows/${id}?embed[]=seasons&embed[]=episodes&embed[]=cast`)
+            .then(results => {
+                setTimeout(() => {
+                    if (isMounted) {
+                        setShow(results);
+                        setIsLoading(false);
+                    }
+                }, 1000);
+            })
+            .catch(err => {
+                if (isMounted) {
+                    setErrors(err.message);
+                    setIsLoading(false);
+                }
+            });
+
+        return () => {
+            isMounted = false;
+        };
     }, [id]);
+
+    if (isLoading) {
+        return <div>Data is being loaded...</div>;
+    }
+
+    if (errors) {
+        return <div>Error Occured : {errors}</div>;
+    }
     return <div>This is show page</div>;
 };
 
