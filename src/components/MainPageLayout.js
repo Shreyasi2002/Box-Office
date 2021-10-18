cc9fff63cf91d83bd30dc73e018c94153c584a59
Shreyasi2002 <shreyasi18sia@gmail.com>
2021-10-19 02:06:29 +0530

    Fragmented MainPageLayout into Title and Navs

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import Navs from './Navs';
-import Title from './Title';
 
 const MainPageLayout = ({ children }) => (
     <div>
-        <Title
-            title="BOX OFFICE"
-            subtitle="Are you looking for a movie or an actor?"
-        />
         <Navs />
         {children}
     </div>
